0c06509b18bac50c59814d66cdf29f78fe9687a1
Seregogy <delhikovsr@gmail.com>
2025-08-26 22:46:38 +0300

    split get artist endpoint at two others

--- src/main/kotlin/routes/artists/GetArtistTopTracks.kt
+++ src/main/kotlin/routes/artists/GetArtistTopTracks.kt
@@ -1,27 +1,29 @@
 package org.example.routes.artists
 
-import io.ktor.http.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 import kotlinx.serialization.Serializable
 import org.example.dto.BaseTrack
-import org.example.dto.FullArtist
 import org.example.dto.toBaseDTO
-import org.example.dto.toFullDTO
 import org.example.model.ArtistEntity
 import org.example.tools.cacheControl
 import org.example.tools.minutes
 import org.example.tools.tryParseUUIDFromString
 import org.jetbrains.exposed.sql.transactions.transaction
 
+const val tracksLimitDefault = 9
+
 @Serializable
-data class GetArtistResponse(
-	val artist: FullArtist = FullArtist()
+data class GetArtistTopTracksResponse(
+	val tracks: List<BaseTrack> = listOf()
 )
 
+fun Route.getArtistTopTracks() {
+	get("{id}/tracks/top") {
+		val tracksLimit = call.queryParameters["limit"]?.toInt() ?: tracksLimitDefault
 
-fun Route.getArtist() {
-	get("{id}") {
 		val artistId = call.parameters["id"]?.let {
 			tryParseUUIDFromString(it)
 		} ?: return@get call.respond(
@@ -40,7 +42,11 @@ fun Route.getArtist() {
 			)
 		)
 
-		call.cacheControl(30.minutes())
-		call.respond(GetArtistResponse(artist.toFullDTO()))
+		val tracks = transaction {
+			artist.albums.flatMap { it.tracks }.sortedByDescending { it.listening }.take(tracksLimit).map { it.toBaseDTO() }
+		}
+
+		call.cacheControl(5.minutes())
+		call.respond(GetArtistTopTracksResponse(tracks))
 	}
 }
