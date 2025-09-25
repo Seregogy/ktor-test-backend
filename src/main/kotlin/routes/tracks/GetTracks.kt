7384bb0ee017b85fd39a3df561557499d90ff8fc
Seregogy <delhikovsr@gmail.com>
2025-09-25 22:21:32 +0300

    some fixes

--- src/main/kotlin/routes/tracks/GetTracks.kt
+++ src/main/kotlin/routes/tracks/GetTracks.kt
@@ -6,23 +6,37 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import kotlinx.serialization.Serializable
+import org.example.dto.FullTrack
+import org.example.dto.toFullDTO
+import org.example.model.TrackEntity
+import org.example.model.TracksTable
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
 
 @Serializable
 data class GetTracksRequest(
 	val tracks: List<String>
 )
 
+@Serializable
+data class GetTracksResponse(
+	val tracks: List<FullTrack>
+)
+
 fun Route.getTracks() {
-	post("tracks") {
+	post("") {
 		val request = call.receive<GetTracksRequest>()
 
 		runCatching {
-			val tracks = transaction {
-				request.tracks.map {
-
+			 transaction {
+				TrackEntity.find {
+					TracksTable.id inList request.tracks.map { UUID.fromString(it) }
+				}.map { track ->
+					track.toFullDTO("${System.getenv("baseUrl")}/audio/${track.id}.mp3")
 				}
 			}
+		}.onSuccess {
+			call.respond(GetTracksResponse(it))
 		}.onFailure {
 			return@post call.respond(
 				status = HttpStatusCode.BadRequest,
